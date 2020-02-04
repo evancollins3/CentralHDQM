05f995cccf027bbd59ca94a6e91fcaba5c0fd6a0
andrius <andrius.kirilovas@cern.ch>
2020-02-04 18:11:08 +0100

    Added a script that will be run by the daily cron job

---
+++ backend/extractor/daily-extraction.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This is executed by the systemd timer every day.
+# This script extracts new data from EOS and populates the HDQM database
+# so no manual extraction has to be done.
+
+source ../cmsenv
+
+# Extract
+export PYTHONPATH="$(cd ../; pwd)/.python_packages/python2"
+./hdqmextract.py
+./calculate.py
+
+# Collect OMS and RR info about runs
+export PYTHONPATH="$(cd ../; pwd)/.python_packages/python3"
+./oms_extractor.py
+./rr_extractor.py
+
+echo "Extraction finished."
